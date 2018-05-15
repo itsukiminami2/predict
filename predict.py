4f57535540f39bb27dc85c5a241dc6f09c48264c
Mayank Kumar <ikki2@live.com>
2018-05-15 06:17:33 +0530

    final(maybe) changes

--- predict.py
+++ predict.py
@@ -16,9 +16,52 @@ TEMPLATE = '''
     <title></title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ url_for('static', filename='styles.css') }}">
+    <style>
+        th, td {
+            border: 1px solid black;
+            padding: 8px;
+        }
+
+        table {
+            border-collapse: collapse;
+            font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+        }
+
+        th {
+            padding-top: 12px;
+            padding-bottom: 12px;
+            text-align: left;
+            background-color: #4CAF50;
+            color: white;
+        }
+
+        tr:nth-child(even){background-color: #f2f2f2;}
+
+        tr:hover {background-color: #ddd;}
+    </style>
 </head>
 <body>
+        <h3>Predictions over the next 4 years :</h3>
+            <table>
+                <thead>
+                    <tr>
+                        <th>2018</th>
+                        <th>2019</th>
+                        <th>2020</th>
+                        <th>2021</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>{{ p[0] }}</td>
+                        <td>{{ p[1] }}</td>
+                        <td>{{ p[2] }}</td>
+                        <td>{{ p[3] }}</td>
+                    </tr>
+                </tbody>
+            </table>
+            <br /><br />
+            <h3>Nominal GDP growth : </h3>
             <img src="{{ url_for('static',filename='plot_' + country + '.png') }}"  width="800" height="400" />
 </body>
 </html>
@@ -33,28 +76,18 @@ def predict():
     quandl.ApiConfig.api_key = "U-FFkY7wcyoehrZnXxWs"
     code = 'ODA/%s_NGDP' % country
     df = quandl.get(code)
+    df = df[:-5]
     df = df.reset_index()
     df.columns = ['ds', 'y']
     
     model = Prophet(weekly_seasonality=True, daily_seasonality=True)
     model.fit(df)
-    future = model.make_future_dataframe(periods=2, freq = 'a')
+    future = model.make_future_dataframe(periods=4, freq = 'a')
     forecast = model.predict(future)
-
-    df.set_index('ds', inplace=True)
-    forecast.set_index('ds', inplace=True)
-
-    df_plot = df.join(forecast[['yhat']], how = 'outer')
-    df_plot = df_plot.reset_index()
-    df_plot.columns = ['year', 'current gdp', 'predicted gdp']
-    df_plot.set_index('year', inplace=True)
-
-    df_plot.plot()
-    fig = plt.gcf()
-    plt.title('Nominal GDP Growth')
-    plt.ylabel('GDP (billions of US$)')
-    fig.savefig('static\plot_%s.png' % country, dpi=100)
-
+    
+    
+    predictions = list(forecast[['yhat']].values.flatten())[-4:]
     return render_template_string(TEMPLATE, **{
-        'country': country
+        'country': country,
+        'p': predictions
     })
